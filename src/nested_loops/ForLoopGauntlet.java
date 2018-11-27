b088fc60e7eb6d388447f445425ee6bbf1b85aaf
598659 <598659@0338S-S-R0B6H0L.sdcs.local>
2018-11-27 14:22:21 -0800

    For Loop Gauntlet

--- src/nested_loops/ForLoopGauntlet.java
+++ src/nested_loops/ForLoopGauntlet.java
@@ -7,32 +7,38 @@ public class ForLoopGauntlet {
 		for (int i = 0; i <= 100; i++) {
 			System.out.println(i);
 		}
+		System.out.println();
 		
 		//2
 		for (int i = 100; i >= 0; i--) {
 			System.out.println(i);
 		}
+		System.out.println();
 		
 		//3
 		for (int i = 2; i <= 100; i++) {
 			if (i%2==0) {System.out.println(i);}
 		}
+		System.out.println();
 		
 		//4
 		for (int i = 1; i <= 99; i++) {
 			if (i%2!=0) {System.out.println(i);}
 		}
+		System.out.println();
 		
 		//5
 		for (int i = 1; i <= 500; i++) {
 			if (i%2!=0) {System.out.println(i + " is odd.");}
 			else {System.out.println(i + " is even.");}
 		}
+		System.out.println();
 		
 		//6
 		for (int i = 0; i <= 777; i++) {
 			if (i%7==0) {System.out.println(i);}
 		}
+		System.out.println();
 		
 		//7
 		int year = 0;
@@ -41,6 +47,7 @@ public class ForLoopGauntlet {
 			System.out.println("In " + i + ", I was " + year + " old");
 			year = year + 1;
 		}
+		System.out.println();
 		
 		//Nested For-Loops
 		//1
@@ -49,9 +56,33 @@ public class ForLoopGauntlet {
 				System.out.println(i + " " + j);
 			}
 		}
+		System.out.println();
 		
 		//2
+		for (int i = 0; i <= 6; i = i + 3) {
+			for (int j = 1; j <= 3; j++) {
+			System.out.print(i + j + " ");
+		}
+			System.out.println();
+		}
+		System.out.println();
 		
+		//3
+		for (int i = 0; i <= 90; i = i + 10) {
+			for (int j = 1; j <= 10; j++) {
+			System.out.print(i + j + " ");
+		}
+			System.out.println();
+		}
+		System.out.println();
+		
+		//4
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
